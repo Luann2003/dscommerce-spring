8d71b2f37f476caf02f5c7eb104d621df879b47b
Luann2003 <lvictorluciano@outlook.com>
2023-07-24 20:11:59 -0300

    first

---
+++ src/main/java/com/devsupeior/dscommerce1/entities/Payment.java
@@ -0,0 +1,11 @@
+package com.devsupeior.dscommerce1.entities;
+
+import java.time.Instant;
+
+public class Payment {
+	
+	private Long id;
+	private Instant moment;
+	
+
+}
---
+++ src/main/java/com/devsupeior/dscommerce1/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.devsupeior.dscommerce1.enums;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED;
+
+}
